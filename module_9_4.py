47070c787a9702fddc34b883dd6e871ee5d9ead2
JuliaRodi <nekrasowayulia2016@gmail.com>
2024-12-06 22:58:24 +0300

    Задача "Функциональное разнообразие":
    Домашнее задание по теме "Создание функций на лету"

---
+++ module_9_4.py
@@ -0,0 +1,36 @@
+# Lambda-функция:
+
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+list_1 = (map(lambda x, y: x == y, first, second))
+
+print(list(list_1))
+
+# Замыкание:
+
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open('example.txt', 'w', encoding='UTF-8') as file:
+            for data in data_set:
+                file.write(str(data) + '\n')
+    return write_everything
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+# Метод __call__:
+
+from random import choice
+
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return choice(self.words)
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
